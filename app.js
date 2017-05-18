9360759d32c4f7d5aca38faa5a2390d1205dac4d
Jain <roopjain@amazon.com>
2017-05-18 21:18:09 +0530

    Modified app.js to create https server

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 // Import required modules
 const express = require('express');
 const bodyParser = require("body-parser");
+const fs = require('fs');
+const https = require('https');
 
 // Initialize express app
 const app = express();
@@ -11,9 +13,11 @@ app.use(bodyParser.json());
 // initialize routes
 app.use(require('./controller/router'));
 
-const PORT = 8080;
+const PORT = 8043;
 
-app.listen(PORT, function () {
+https.createServer({
+    key: fs.readFileSync('./certs/key.pem'),
+    cert: fs.readFileSync('./certs/cert.pem')
+}, app).listen(PORT, function () {
     console.log('Server is running on port: ' + PORT);
 });
-
